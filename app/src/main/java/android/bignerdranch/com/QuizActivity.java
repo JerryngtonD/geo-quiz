3af6c8be1ab1fd7b9da015c2890a58142323a84c
jerryngton <jerryngtond@gmail.com>
2019-08-16 08:26:43 +0300

    init first project

---
+++ app/src/main/java/android/bignerdranch/com/QuizActivity.java
@@ -0,0 +1,23 @@
+package android.bignerdranch.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class QuizActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+
+    }
+}
