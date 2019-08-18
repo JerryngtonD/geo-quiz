104fcd46e65289ad26120840a8af23007903601f
jerryngton <jerryngtond@gmail.com>
2019-08-18 13:50:21 +0300

    added question model

---
+++ app/src/main/java/android/bignerdranch/com/Question.java
@@ -0,0 +1,28 @@
+package android.bignerdranch.com;
+
+public class Question {
+    private int mTextRestId;
+    private boolean mAnswerTrue;
+
+
+    public Question(int textRestId, boolean answerTrue) {
+        mTextRestId = textRestId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextRestId() {
+        return mTextRestId;
+    }
+
+    public void setTextRestId(int textRestId) {
+        mTextRestId = textRestId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
